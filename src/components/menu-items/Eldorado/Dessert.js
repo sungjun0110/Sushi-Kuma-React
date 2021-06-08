93f42a4abe533dca6055a660413470e34b6cb2bc
sungjun0110 <55672234+sungjun0110@users.noreply.github.com>
2021-06-08 03:32:35 -0700

    Update menu

--- src/components/menu-items/Eldorado/Dessert.js
+++ src/components/menu-items/Eldorado/Dessert.js
@@ -7,12 +7,12 @@ const Dessert = ( props ) => {
         {CreateItems(
           "Dessert", 
           {
-          "Soda" : ["Coke, Diet Coke, Mr.pibb, Lemonade, Fanta, Sprite", "2.50"], 
-          "Ramune" : ["Japanese marble soda", "2.99"], 
-          "Iced Tea" : ["", "2.50"], 
-          "Green Iced Tea" : ["", "2.50"], 
-          "Hot Tea" : ["Genmai", "1.00"], 
-          "Mochi Ice cream" : ["strawberry, chocolate, mango", "3.99"], 
+          "Soda" : ["Coke, Diet Coke, Mr.pibb, Lemonade, Fanta, Sprite", "2.95"], 
+          "Ramune" : ["Japanese marble soda", "3.50"], 
+          "Iced Tea" : ["", "2.95"], 
+          "Green Iced Tea" : ["", "2.75"], 
+          "Hot Tea" : ["Genmai", "1.99"], 
+          "Mochi Ice cream" : ["strawberry, chocolate, mango", "4.50"], 
         }, props)}
     </div>
   );
