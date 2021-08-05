7eb6ac1950e96931b4ba6065fb9e748de597929e
sungjun0110 <55672234+sungjun0110@users.noreply.github.com>
2021-08-04 23:03:31 -0700

    Added Darkmode context

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+import React, { useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Page from './components/Page';
 
+export const DarkModeContext = React.createContext();
+
 function App() {
+  const DarkModeState = useState(true);
+
   return (
-    <div className="App">
-      <Header />
-      <Page />
-    </div>
+    <DarkModeContext.Provider value={DarkModeState}>
+      <div className="App">
+        <Header />
+        <Page />
+      </div>
+    </DarkModeContext.Provider>
   );
 }
 
