c9fc35fd50f51aeae7d04269af167a4700b1eb8b
sungjun0110 <55672234+sungjun0110@users.noreply.github.com>
2021-08-05 23:38:34 -0700

    Added NavActiveContext

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import Header from './components/Header';
 import Page from './components/Page';
 
 export const DarkModeContext = React.createContext();
+export const NavActiveContext = React.createContext();
 
 function App() {
   const DarkModeState = useState(true);
+  const NavActiveState = useState(false);
 
   return (
     <DarkModeContext.Provider value={DarkModeState}>
-      <div className="App">
-        <Header />
-        <Page />
-      </div>
+      <NavActiveContext.Provider value={NavActiveState}>
+        <div className="App">
+          <Header />
+          <Page />
+        </div>
+      </NavActiveContext.Provider>
     </DarkModeContext.Provider>
   );
 }
