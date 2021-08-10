b59ba198f14230a225851fc65aa2e9520ca3305c
sungjun0110 <55672234+sungjun0110@users.noreply.github.com>
2021-08-09 23:24:54 -0700

    added NavUrlContext

--- src/App.js
+++ src/App.js
@@ -5,18 +5,22 @@ import Page from './components/Page';
 
 export const DarkModeContext = React.createContext();
 export const NavActiveContext = React.createContext();
+export const NavUrlContext = React.createContext();
 
 function App() {
   const DarkModeState = useState(true);
   const NavActiveState = useState(false);
+  const NavUrlState = useState('');
 
   return (
     <DarkModeContext.Provider value={DarkModeState}>
       <NavActiveContext.Provider value={NavActiveState}>
-        <div className="App">
-          <Header />
-          <Page />
-        </div>
+        <NavUrlContext.Provider value={NavUrlState}>
+          <div className="App">
+            <Header />
+            <Page />
+          </div>
+        </NavUrlContext.Provider>
       </NavActiveContext.Provider>
     </DarkModeContext.Provider>
   );
