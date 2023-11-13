1340f597db904dcda9a3c665dfabd4d10a122a15
Choi <schoi@ip-192-168-1-190.ec2.internal>
2023-11-12 20:04:23 -0800

    update eldorado kids menu

--- src/components/menu-items/Eldorado/Kids.js
+++ src/components/menu-items/Eldorado/Kids.js
@@ -1,19 +1,30 @@
-import React from 'react';
-import CreateItems from '../CreateItems';
+import React from "react";
+import CreateItems from "../CreateItems";
 
-const Kids = ( props ) => {
-  return(
-    <div className="item-div">
-      {CreateItems(
-        "Kids' Menu", 
-        {
-        "Teddy Bear 1" : ["Chicken teriyaki or Sesame chicken, California roll – 4pcs, gyoza - 2pcs", "11.95"], 
-        "Teddy Bear 2" : ["Mixed tempura, California roll – 4pcs, gyoza - 2pcs", "11.95"], 
-        "Teddy Bear 3" : ["Chicken Katsu or Ton Katsu, California – 4pcs, gyoza - 2pcs", "11.95"], 
-        "Kid's Udon" : ["Thick noddle soup", "10.95"], 
-      }, props)}
-    </div>
-  );
-}
+const Kids = (props) => {
+    return (
+        <div className="item-div">
+            {CreateItems(
+                "Kids' Menu",
+                {
+                    "Teddy Bear 1": [
+                        "Chicken teriyaki or Sesame Chicken, Mixed Tempura, Gyoza - 2pcs",
+                        "11.95"
+                    ],
+                    "Teddy Bear 2": [
+                        "Chicken teriyaki or Sesame Chicken, California roll – 4pcs, gyoza - 2pcs",
+                        "11.95"
+                    ],
+                    "Teddy Bear 3": [
+                        "Chicken teriyaki or Sesame Chicken, Croquette - 1pc, gyoza - 2pcs",
+                        "11.95"
+                    ],
+                    "Kid's Udon": ["Thick noddle soup", "10.95"]
+                },
+                props
+            )}
+        </div>
+    );
+};
 
 export default Kids;
