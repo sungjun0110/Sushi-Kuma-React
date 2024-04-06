57dca8fa3709788433afc3359f36bb2cc6403fa3
Choi <schoi@ip-192-168-1-190.ec2.internal>
2024-04-06 12:52:31 -0700

    add warning message

--- src/components/navbar/Navbtn.js
+++ src/components/navbar/Navbtn.js
@@ -1,45 +1,72 @@
-import React, { useState, useEffect, useContext } from 'react';
-import './Navbtn.css';
+import React, { useState, useEffect, useContext } from "react";
+import "./Navbtn.css";
 import { Link } from "react-router-dom";
-import { NavActiveContext } from '../../App';
+import { NavActiveContext } from "../../App";
 
-const clickHandler = ( onClick, setClicked, btnClicked, btnName, setActiveDir ) => {
-  onClick();
-  setClicked(isClicked => true);
-  btnClicked[1](btnName);
-}
+const clickHandler = (
+    onClick,
+    setClicked,
+    btnClicked,
+    btnName,
+    setActiveDir
+) => {
+    onClick();
+    setClicked((isClicked) => true);
+    btnClicked[1](btnName);
+};
 
-const Navbtn = ( props ) => {
-  const [isClicked, setClicked] = useState(false);
-  const [classNames, setClassNames] = useState('');
-  const [isNavActive,] = useContext(NavActiveContext);
+const Navbtn = (props) => {
+    const [isClicked, setClicked] = useState(false);
+    const [classNames, setClassNames] = useState("");
+    const [isNavActive] = useContext(NavActiveContext);
 
-  let btnVal = '/' + props.value.toLowerCase().replace(/ /g, '');
+    let btnVal = "/" + props.value.toLowerCase().replace(/ /g, "");
 
-  useEffect(() => {
-    if (isNavActive) {
-      setClassNames(`active ${(isClicked && props.value === props.btnClicked[0])? "clicked" : null}`);
-    } else {
-      setClassNames('');
-    }
+    useEffect(() => {
+        if (isNavActive) {
+            setClassNames(
+                `active ${
+                    isClicked && props.value === props.btnClicked[0]
+                        ? "clicked"
+                        : null
+                }`
+            );
+        } else {
+            setClassNames("");
+        }
 
-    let address = window.location.pathname.slice(1);
-    if (address === 'roseville' || address === 'eldoradohills') address = 'menu';
-    if (address === props.value.toLowerCase().replace(/ /g, '')) {
-      clickHandler(props.onClick, setClicked, props.btnClicked, props.value);
-    }
-  });
+        let address = window.location.pathname.slice(1);
+        if (address === "roseville" || address === "eldoradohills")
+            address = "menu";
+        if (address === props.value.toLowerCase().replace(/ /g, "")) {
+            clickHandler(
+                props.onClick,
+                setClicked,
+                props.btnClicked,
+                props.value
+            );
+        }
+    });
 
-  return (
-    <Link to={btnVal}>
-      <button id='navBtn'
-        className={classNames}
-        onClick={() => clickHandler(props.onClick, setClicked, props.btnClicked, props.value, props.setActiveDir)} >
-          {props.value}
-      </button>
-    </Link>
-  );
-    
-}
+    return (
+        <Link to={btnVal}>
+            <button
+                id="navBtn"
+                className={classNames}
+                onClick={() =>
+                    clickHandler(
+                        props.onClick,
+                        setClicked,
+                        props.btnClicked,
+                        props.value,
+                        props.setActiveDir
+                    )
+                }
+            >
+                {props.value}
+            </button>
+        </Link>
+    );
+};
 
 export default Navbtn;
--- src/components/pages/EldoradoMenu.js
+++ src/components/pages/EldoradoMenu.js
@@ -1,78 +1,114 @@
-import React, { useReducer, useState } from 'react';
-import Category from '../menu-items/Category';
-import { Link } from 'react-router-dom';
+import React, { useReducer, useState } from "react";
+import Category from "../menu-items/Category";
+import { Link } from "react-router-dom";
 
-import App from '../menu-items/Eldorado/Appetizer';
-import Bev from '../menu-items/Eldorado/Beverage';
-import Des from '../menu-items/Eldorado/Dessert';
-import Ent from '../menu-items/Eldorado/Entree';
-import Kid from '../menu-items/Eldorado/Kids';
-import Nigi from '../menu-items/Eldorado/Nigiri';
-import Roll from '../menu-items/Eldorado/Rolls';
-import Side from '../menu-items/Eldorado/Sides';
-import Spe from '../menu-items/Eldorado/SpecialRolls';
-import Com from '../menu-items/Eldorado/SushiCombo';
-import DinnerBento from '../menu-items/Eldorado/DinnerBento';
-import LunchBento from '../menu-items/Eldorado/LunchBento';
+import App from "../menu-items/Eldorado/Appetizer";
+import Bev from "../menu-items/Eldorado/Beverage";
+import Des from "../menu-items/Eldorado/Dessert";
+import Ent from "../menu-items/Eldorado/Entree";
+import Kid from "../menu-items/Eldorado/Kids";
+import Nigi from "../menu-items/Eldorado/Nigiri";
+import Roll from "../menu-items/Eldorado/Rolls";
+import Side from "../menu-items/Eldorado/Sides";
+import Spe from "../menu-items/Eldorado/SpecialRolls";
+import Com from "../menu-items/Eldorado/SushiCombo";
+import DinnerBento from "../menu-items/Eldorado/DinnerBento";
+import LunchBento from "../menu-items/Eldorado/LunchBento";
 
 const EldoradoMenu = () => {
-  const reducer = (items, action) => {
-    switch (action) {
-      case 'add':
-        let temp = [...items, {id: itemId , name: item.name, description: item.description, price: item.price}];
-        setItemId(itemId+1);
-        return temp;
-      case 'remove':
-        return items.filter(i => i.id !== item.id);
-      default:
-        return items;
+    const reducer = (items, action) => {
+        switch (action) {
+            case "add":
+                let temp = [
+                    ...items,
+                    {
+                        id: itemId,
+                        name: item.name,
+                        description: item.description,
+                        price: item.price
+                    }
+                ];
+                setItemId(itemId + 1);
+                return temp;
+            case "remove":
+                return items.filter((i) => i.id !== item.id);
+            default:
+                return items;
+        }
+    };
+
+    const [itemId, setItemId] = useState(0);
+    const [item, setItem] = useState({});
+    const [, dispatch] = useReducer(reducer, []);
+
+    // categori name : page element
+    const cateDict = {
+        Appetizer: <App setItem={setItem} dispatch={() => dispatch("add")} />,
+        Nigiri: <Nigi setItem={setItem} dispatch={() => dispatch("add")} />,
+        "Lunch Bento Box": (
+            <LunchBento setItem={setItem} dispatch={() => dispatch("add")} />
+        ),
+        "Dinner Bento Box": (
+            <DinnerBento setItem={setItem} dispatch={() => dispatch("add")} />
+        ),
+        Entrée: <Ent setItem={setItem} dispatch={() => dispatch("add")} />,
+        "Kids' Menu": (
+            <Kid setItem={setItem} dispatch={() => dispatch("add")} />
+        ),
+        "Sushi & Sashimi Combo": (
+            <Com setItem={setItem} dispatch={() => dispatch("add")} />
+        ),
+        Rolls: <Roll setItem={setItem} dispatch={() => dispatch("add")} />,
+        "Kuma Special Rolls": (
+            <Spe setItem={setItem} dispatch={() => dispatch("add")} />
+        ),
+        Side: <Side setItem={setItem} dispatch={() => dispatch("add")} />,
+        "Alcoholic Beverage": (
+            <Bev setItem={setItem} dispatch={() => dispatch("add")} />
+        ),
+        "Beverage & Dessert": (
+            <Des setItem={setItem} dispatch={() => dispatch("add")} />
+        )
+    };
+
+    const categories = [];
+
+    for (const [catName, page] of Object.entries(cateDict)) {
+        categories.push(
+            <Category name={catName} key={catName}>
+                {page}
+            </Category>
+        );
     }
-  }
 
-  const [itemId, setItemId] = useState(0);
-  const [item, setItem] = useState({});
-  const [, dispatch] = useReducer(reducer, []);
+    return (
+        <>
+            <div className="page">
+                <p className="scheduleWarning">
+                    *** For the next 5 weeks, our restaurant will be closed on
+                    Tuesdays. We appreciate your understanding. ***
+                </p>
+                <div id="title">El Dorado Hills</div>
 
-  // categori name : page element
-  const cateDict = {
-    'Appetizer': <App setItem={setItem} dispatch={() => dispatch('add')} />, 
-    'Nigiri': <Nigi setItem={setItem} dispatch={() => dispatch('add')} />, 
-    'Lunch Bento Box': <LunchBento setItem={setItem} dispatch={() => dispatch('add')} />, 
-    'Dinner Bento Box': <DinnerBento setItem={setItem} dispatch={() => dispatch('add')} />, 
-    'Entrée': <Ent setItem={setItem} dispatch={() => dispatch('add')} />, 
-    "Kids' Menu": <Kid setItem={setItem} dispatch={() => dispatch('add')} />, 
-    'Sushi & Sashimi Combo': <Com setItem={setItem} dispatch={() => dispatch('add')} />, 
-    'Rolls': <Roll setItem={setItem} dispatch={() => dispatch('add')} />, 
-    'Kuma Special Rolls': <Spe setItem={setItem} dispatch={() => dispatch('add')} />, 
-    'Side': <Side setItem={setItem} dispatch={() => dispatch('add')} />, 
-    'Alcoholic Beverage': <Bev setItem={setItem} dispatch={() => dispatch('add')} />, 
-    'Beverage & Dessert': <Des setItem={setItem} dispatch={() => dispatch('add')} />, 
-  }
+                <div className="warning">
+                    <p>
+                        *Consuming raw or undercooked meats, poultry, seafood,
+                        shellfish, or eggs may increase your risk of foodborne
+                        illness.
+                        <br />
+                        *Our menu and prices are subject to change.
+                        <br />
+                        *18% gratuity will be added for parties of six or more.
+                    </p>
+                </div>
+                {categories}
 
-  const categories = [];
-
-  for (const [catName, page] of Object.entries(cateDict)) {
-    categories.push(<Category name={catName} key={catName}>{page}</Category>);
-  }
-
-  return (
-    <>
-      <div className='page'>
-        <div id='title'>
-            El Dorado Hills
-        </div>
-
-        <div className='warning'>
-          <p>*Consuming raw or undercooked meats, poultry, seafood, shellfish, or eggs may increase your risk of foodborne illness.<br />*Our menu and prices are subject to change.<br />*18% gratuity will be added for parties of six or more.</p>
-        </div>
-        {categories}
-
-        <Link to='/menu'>
-          <button id='back-btn'>Back</button>
-        </Link>
-      </div>
-    </>
-  );
-}
+                <Link to="/menu">
+                    <button id="back-btn">Back</button>
+                </Link>
+            </div>
+        </>
+    );
+};
 
 export default EldoradoMenu;
